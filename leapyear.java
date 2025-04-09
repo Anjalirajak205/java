05ae39754d44ee23879a378af76f4664b2611540
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-09 22:20:39 +0530

    java class1

---
+++ leapyear.java
@@ -0,0 +1,13 @@
+public class leapyear {
+        public static void main(String[] args) {
+            int year = 2025;
+    
+            if (year % 4 == 0 && (year % 100 != 0 || year % 400 == 0)) {
+                System.out.println("Leap year");
+            } else {
+                System.out.println("Not a leap year");
+            }
+        }
+    }
+    
+
