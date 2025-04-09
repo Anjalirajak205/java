5aa201e564a2fe74ddfabddc686ba23b4ba6d72f
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-09 11:30:16 +0530

    java class1

---
+++ largest.java
@@ -0,0 +1,15 @@
+public class largest {
+    public static void main(String[] args){
+        int a=20 , b=30 ;
+        if (a>b)
+            System.out.println("a is greater");
+        else
+            System.out.println("b is greater");
+
+
+        }
+        
+         
+    }
+    
+
