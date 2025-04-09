36d4434fd7348dcefc9dbb18f826e999388791f3
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-09 11:19:18 +0530

    java class1

---
+++ basic.java
@@ -0,0 +1,28 @@
+public class basic{
+    public static void main(String[] args){
+        System.out.println("Hello World");
+
+
+       // byte a=20;
+       // byte b=30;
+       // int  c =a+b;
+
+       // boolean a=true;
+       // boolean b=false;
+       // System.out.println(a||b);
+
+       // int a=5;
+        //int b=4;
+       // System.out.println(a&b); // bit operator
+
+      // System.out.println(5>6?"hii":"hello"); //if true print- hii and if false- hello
+     boolean a=false;
+     if (a==true){
+         System.out.println("true");
+     }
+     else {
+        System.out.println("false");
+     }
+    }} 
+      
+    
---
+++ operator.java
@@ -0,0 +1,39 @@
+public class operator {
+    public static void main(String[] args){
+        
+        //int a=10;
+        //int b=5;
+        //System.out.println("Addition:"+(a+b));
+        //System.out.println("Substraction:"+(a-b));
+       // System.out.println("Division:"+(a/b));
+       // System.out.println("multiplication:"+(a*b));
+
+       // remainder using modulus operator
+       /*int a=159;
+       int b=5;
+       System.out.println("Remainder:"+(a%b));
+        */
+
+
+        //swap numbers
+        /* 
+        int a=4;
+        int b=6;
+        int temp=a;
+        a=b;
+        b=temp;
+        System.out.println("a="+a+",b="+b);
+        */
+
+        // whether a number is even or odd
+       /* 
+        int num =10;
+        if(num%2==0)
+           System.out.println("even");
+        else
+           System.out .println("odd"); 
+        */ 
+}
+
+        
+    }
