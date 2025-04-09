d66fbb038464c4fa9150808a7b517600e30cc94f
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-09 22:13:53 +0530

    java class1

---
+++ largerofthree.java
@@ -0,0 +1,16 @@
+public class largerofthree {
+        public static void main(String[] args) {
+            int a = 30, b = 5, c = 10;
+    
+            if (a > b && a > c) {
+                System.out.println("a is greatest");
+            } else if (b > a && b > c) {
+                System.out.println("b is greatest");
+            } else {
+                System.out.println("c is greatest");
+            }
+        }
+    }
+    
+    
+
