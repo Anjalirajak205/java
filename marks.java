fe628219ecae0636693666033d16a35f24b1c0fe
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-10 21:03:12 +0530

    java class1

---
+++ marks.java
@@ -0,0 +1,13 @@
+public class marks {
+    public static void main(String[] args){
+        int marks = 90;
+        if ( marks>=95)
+            System.out.println("Grade A");
+        else if (marks>=80)
+            System.out.println("Grade B");
+        else if (marks>=50)
+            System.out.println("Grade C");
+        else
+            System.out.println("fail");    
+    }
+}
