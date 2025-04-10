23ea80fb43070168c299b1821da25a5dcb161fb5
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-10 20:45:31 +0530

    java class1

---
+++ vowels.java
@@ -0,0 +1,9 @@
+public class vowels {
+    public static void main(String[] args){
+        char ch = 'p';
+        if (ch == 'a' || ch == 'e' || ch == 'i' || ch == 'o' || ch == 'u')
+           System.out.println("Vowel");
+        else
+           System.out.println("Consonant");
+    }
+}
