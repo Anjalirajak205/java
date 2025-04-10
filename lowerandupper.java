8da04814f0959e2cf7854ca2b49f6782240b57ad
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-10 21:14:37 +0530

    java class1

---
+++ lowerandupper.java
@@ -0,0 +1,12 @@
+public class lowerandupper {
+    public static void main(String[] args){
+        char ch = 'P';
+ if (ch >= 'A' && ch <= 'Z')
+    System.out.println("Uppercase");
+ else if (ch >= 'a' && ch <= 'z')
+    System.out.println("Lowercase");
+ else
+    System.out.println("Not an alphabet");
+        
+    }
+}
