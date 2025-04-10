f930393e311557e84348a0c35d44957ed749ee02
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-10 20:53:56 +0530

    java class1

---
+++ eligibilityforvote.java
@@ -0,0 +1,12 @@
+public class eligibilityforvote {
+    public static void main(String[] args){
+        int age = 20;
+        if (age>=18){
+            System.out.println("eligible to vote");
+        }
+        else{
+            System.out.println("not eligible");
+        }
+        }
+    }
+
