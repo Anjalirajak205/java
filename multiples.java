f44577d29b610d9e553ef84ee5b8fac3ebd40a13
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-11 19:49:40 +0530

    7:49

---
+++ multiples.java
@@ -0,0 +1,26 @@
+import java.util.Scanner;
+
+public class multiples {
+    public static void main(String[] args){
+        System.out.println("Enter a number");
+        Scanner in = new Scanner(System.in);
+        int n = in.nextInt();
+        /* 
+        for (int i = 1; i <= 10; i++) {
+            if (n % i == 0) {
+                System.out.println(n + " is a multiple of " + i);
+                }
+                */
+
+                if (n % 3 == 0) {
+                    System.out.println(n + " is a multiple of 3.");
+                } else {
+                    System.out.println(n+ " is not a multiple of 3.");
+                }
+        
+        
+                }
+                }
+
+    
+
