a2470734f8c54274261bd2878ead65e3d8d5d8e4
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-16 19:20:27 +0530

    array 3

---
+++ linear_array.java
@@ -0,0 +1,30 @@
+import java.util.Scanner;
+
+public class linear_array {
+    public static void main(String[] args) {
+    
+ 
+
+     Scanner in = new Scanner(System.in);
+     System.out.print("Enter size of array: ");
+     int n = in.nextInt();
+     int[] arr = new int[n];
+     System.out.println("Enter elements:");
+     for (int i = 0; i < n; i++) {
+         arr[i] = in.nextInt();
+     }
+     System.out.print("Enter number to search: ");
+     int num = in.nextInt();
+     boolean found = false;
+     for (int val : arr) {
+         if (val == num) {
+             found = true;
+             break;
+         }
+     }
+     if (found)
+         System.out.println(num + " is present in the array.");
+     else
+         System.out.println(num + " is not present in the array.");
+ }
+ }
