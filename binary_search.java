4e793c7126f86e335370b91e24941da622642f79
Anjalirajak205 <anjalirajak205@gamil.com>
2025-04-28 19:54:29 +0530

    28April2025

---
+++ binary_search.java
@@ -0,0 +1,40 @@
+ public class binary_search {
+
+    
+    public static int BinarySearch(int[] arr, int target) {
+        int start = 0;
+        int end = arr.length - 1;
+
+        while (start <= end) {
+            int mid = start + (end - start) / 2;
+
+            if (arr[mid] == target) {
+                return mid;  
+            }
+
+            if (arr[mid] > target) {
+                end = mid - 1;  
+            } else {
+                start = mid + 1;  
+            }
+        }
+
+        return -1;  
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {1, 2, 3, 4, 5, 6, 7, 8};  
+        int target = 33;  
+
+    
+        int result = BinarySearch(arr, target);
+
+        
+        if (result == -1) {
+            System.out.println("Element not found");
+        } else {
+            System.out.println("Element found at index: " + result);
+        }
+    }
+}
+
