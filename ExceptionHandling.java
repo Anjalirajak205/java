a4ef0c700de4e13f1d8e9b9192c8fb1294067a9e
Anjalirajak205 <anjalirajak205@gamil.com>
2025-05-22 20:05:28 +0530

    22 may

---
+++ ExceptionHandling.java
@@ -0,0 +1,46 @@
+import  java.util.Scanner;
+
+public class ExceptionHandling {
+    public static void main(String[] args) {
+       // int a = 10;
+       // int b = 0;
+        Scanner in = new Scanner(System.in);
+       /*System.out.println("enter numbers:");
+       int a=in.nextInt();
+       int b=in.nextInt();
+
+        try{
+            int c = a/b;
+            System.out.println(c);
+        }
+        catch (Exception e){
+            System.out.println("we cannot divide by zero");
+
+
+        }
+        System.out.println("hello");*/
+           System.out.println("enter the size of array");
+            int n = in.nextInt();
+            int [] arr = new int[n];
+            System.out.println("enter the elements of array:");
+            for(int i=0;i<arr.length;i++){
+                arr[i]=in.nextInt();
+            }
+            try{
+                System.out.println("enter the valuee for access:");
+                int i= in.nextInt();
+                System.out.println(arr[i]);
+            }
+            catch(Exception e ){
+                System.out.println("we cannot access the elemtent");
+
+            }
+            finally{
+                System.out.println("sab kuch thik hai");
+            }
+        }
+        
+    }
+    
+    
+
