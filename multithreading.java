fe499333dafcbea0eb58476d30380ede42a93c4f
Anjalirajak205 <anjalirajak205@gamil.com>
2025-06-03 11:36:25 +0530

    03 June

---
+++ multithreading.java
@@ -0,0 +1,23 @@
+
+    class MyThread extends Thread {
+        public void run() {
+            System.out.println("hello");
+            try{
+                Thread.sleep(5000);
+            }
+            catch(Exception e){
+ }
+            System.out.println("hii");
+            }
+            }
+        public class multithreading {    
+            public static void main(String[] args) {
+                MyThread t = new MyThread();
+                t.start();
+                System.out.println("hii");
+            }
+        }
+    
+
+    
+
---
+++ multithreading1.java
@@ -0,0 +1,59 @@
+// Problem
+// class downloadFile{
+//     public static void file(String file) {
+//         for(int i=1;i<=5;i++){
+//             System.out.println(file + " downloading..."+(i*20)+ "%");
+            
+//             try{
+//                 Thread.sleep(2000);
+//             }
+//             catch(Exception e){
+//             }
+//         }
+    
+//              System.out.println(file + " done");
+//           }
+//         }
+        
+// public class multithreading1 {
+//     public static void main(String[] args) {
+//         downloadFile obj = new downloadFile();
+//         obj.file("file A");
+//         obj.file("file B");
+//     }
+// }
+
+// Solution
+class Filedownload extends Thread{
+    String filename;
+    public Filedownload(String filename){
+        this.filename = filename;
+        }
+        public void run(){
+            for(int i=0;i<=5;i++ ){
+                System.out.println(filename + "downloading..." + (i*20)+ "%");
+                try{
+                    Thread.sleep(2000);
+                    }
+                    catch(Exception e){
+                        }
+                        }
+                        System.out.println(filename + "done");
+                        }
+                        }
+        public class multithreading1 {
+            public static void main(String[] args) {
+                Filedownload t1 = new Filedownload("file A");
+                Filedownload t2 = new Filedownload("file B");
+                t1.start();
+                t2.start();
+            }
+
+
+            }
+
+
+
+
+
+
