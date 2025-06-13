3cf3d48d71bda6774ec33639253c11f6cca1ccde
Anjalirajak205 <anjalirajak205@gmail.com>
2025-06-13 23:36:33 +0530

    13 june

---
+++ arithematicOperations.java
@@ -0,0 +1,15 @@
+public class arithematicOperations {
+    
+    public static void main(String[] args) {
+        int x;
+        int y;
+        x = 10;
+        y = 2;
+        System.out.println(x+y);
+        System.out.println(x-y);
+        System.out.println(x*y);
+        System.out.println(x/y);
+    }
+}
+    
+
