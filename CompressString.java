1c450234ceab6fbac829369ce07f99695fd1b0dc
Anjalirajak205 <anjalirajak205@gmail.com>
2025-07-13 21:28:23 +0530

    13 july

--- CompressString.java
+++ CompressString.java
@@ -1,5 +1,5 @@
 public class CompressString {
-    public static void main(String[] args) {
+     public static void main(String[] args) {
         String str ="hhheeelllooo ";
         StringBuilder newStr = new StringBuilder();
         int count =1;
